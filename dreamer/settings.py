0ce0e43fb67a120f18df1a690c5cc53560ba85c3
Joel <aholoujoel5@gmail.com>
2022-09-02 22:08:52 +0000

    Setting fixed

--- dreamer/settings.py
+++ dreamer/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-orh!cc5=li479sn=$jk)8b3lw^#sql!7v9a-ie=nb_=x0j6-i_'
+SECRET_KEY = os.environ.get('DREAMER_SECRET_PASS')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
