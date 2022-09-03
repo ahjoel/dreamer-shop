4f8f216fd8eeee7c542119f4aa5717cadf953dba
Joel <aholoujoel5@gmail.com>
2022-09-03 13:35:32 +0000

    Link source

--- dreamer/settings.py
+++ dreamer/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-if os.environ.get('prod') == 1:
+if config('prod') == 1:
     DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'dreamer-shop.herokuapp.com']
