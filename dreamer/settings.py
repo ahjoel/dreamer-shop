0936614e891ddf4de9ea0d24a6b2bdd13c21401a
Joel <aholoujoel5@gmail.com>
2022-09-05 15:31:25 +0000

    Settings fixed

--- dreamer/settings.py
+++ dreamer/settings.py
@@ -29,7 +29,8 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-# DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
+if config('prod') == 1:
+    DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'dreamer-shop.herokuapp.com']
 
@@ -87,9 +88,8 @@ WSGI_APPLICATION = 'dreamer.wsgi.application'
 
 # if os.environ.get('prod') == 1:
 import dj_database_url
+DATABASES['default'] = dj_database_url.config()
 
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
